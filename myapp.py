8ffbade7ce017ddf4b604f2b4d0ee2e5b7522760
Nikitha Thomas <nikithamarythomas@gmail.com>
2024-07-04 23:44:24 -0400

    Delete myapp.py

--- myapp.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__) #creating the Flask class object
-@app.route('/') #decorator
-def home():
-    return "Hello! Welcome to my flask";
-if __name__ =='__main__':
-    app.run()
